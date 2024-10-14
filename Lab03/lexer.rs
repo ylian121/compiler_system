3f2a71cfbb32b1cccbba804bef36e1ecca5c130f
Yongyan Liang <ylian121@xe-10.cs.ucr.edu>
2024-10-14 15:47:34 -0700

    token

---
+++ Lab03/lexer.rs
@@ -0,0 +1,11 @@
+#[derive(Debug)]
+
+enum Tok {
+    func, return, int, print, read, while, if,
+    else, break, continue,
+    "(", ")", "}", "{", "[", "]", ",", ";",
+    "+", "-", "*", "/", "%", "=", "<", "<=",
+    ">", ">=", "==", "!=",
+
+}
+
