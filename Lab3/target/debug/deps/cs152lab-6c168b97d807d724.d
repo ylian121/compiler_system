94f9a32eeed13714fe7f642a29b7ab7aa4318f0b
LevyH13 <114446224+LevyH13@users.noreply.github.com>
2024-10-28 15:23:13 -0700

    Changed lab folder naming scheme
    Changed naming schemes to match
    lab01 -> Assigning Token
    lab02 -> Lexer
    lab03 -> parser

--- Lab3/target/debug/deps/cs152lab-6c168b97d807d724.d
+++
@@ -1,5 +0,0 @@
-C:\Users\levyh\OneDrive\Documents\GitHub\b\Lab3\target\debug\deps\libcs152lab-6c168b97d807d724.rmeta: src/main.rs
-
-C:\Users\levyh\OneDrive\Documents\GitHub\b\Lab3\target\debug\deps\cs152lab-6c168b97d807d724.d: src/main.rs
-
-src/main.rs:
